abe7e1a3b714b0582d1ff5b41d77f9d93d711e82
dannielccw <dannielccw@gmail.com>
2017-04-20 09:39:12 +0800

    按 按鈕 寫入或讀取檔案至"mydata.txt"

---
+++ app/src/main/java/com/example/yvtc/s042001/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.yvtc.s042001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    File myFile;
+    EditText ed;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myFile = new File(getFilesDir() + File.separator + "mydata.txt");
+        ed = (EditText) findViewById(R.id.editText);
+        tv = (TextView) findViewById(R.id.textView2);
+    }
+
+    public void clickWrite(View v)
+    {
+
+        try {
+            FileWriter fw = new FileWriter(myFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(ed.getText().toString());
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void clickRead(View v)
+    {
+        try {
+            FileReader fr = new FileReader(myFile);
+            BufferedReader br = new BufferedReader(fr);
+            String str = br.readLine();
+            tv.setText(str);
+            br.close();
+            fr.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
